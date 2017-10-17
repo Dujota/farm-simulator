1888c869a2e0783ccc3ad31f8262023bca9647a4
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-16 22:47:11 -0400

    constructed the farm project, working on getting the menu working

--- app.rb
+++ app.rb
@@ -1,36 +1,52 @@
 require_relative 'farm'
+require_relative 'field'
 # Options:
 # field -> adds a new field
 # harvest -> harvests crops and adds to total harvested
 # status -> displays some information about the farm
 # relax -> provides lovely descriptions of your fields
 # exit -> exits the program
+class App < Farm
+  attr_accessor :name
 
-def main_menu
-   while true #repeat indefinitely - inifinite loop
-     print_main_menu # calls print_main_menu method
-     user_selected = gets.to_i #stores user input into a local variable
-     call_option(user_selected) #passes local variable to call_option mehtod below
-   end
- end
-
-def print_main_menu
-    puts '[1] Add new contact'
-    puts '[2] Modify an existing contact'
-    puts '[3] Delete a contact'
-    puts '[4] Display all contacts'
-    puts '[5] Search by attributes'
-    puts '[6] Exit'
-    puts 'Enter a number: '
+  def initialize(name)
+    @name = name
   end
 
-  def call_option(user_selected)
-      case user_selected
-      when 1 then add_new_contact
-      when 2 then modify_existing_contact
-      when 3 then delete_contact
-      when 4 then display_all_contacts
-      when 5 then search_by_attribute
-      when 6 then exit(0)  # can also do else exit as the last condition, up to programmer
-      end
+  def name_of_app
+    "This application is called " + @name
+  end
+
+  def main_menu
+    loop do # repeat indefinitely - inifinite loop
+      print_main_menu # calls print_main_menu method
+      user_selected = gets.to_i # stores user input into a local variable
+      call_option(user_selected) # passes local variable to call_option mehtod below
     end
+   end
+
+  def print_main_menu
+    puts '[1] Add a field'
+    puts '[2] Harvest your fields'
+    puts '[3] Status of Fields'
+    puts '[4] Just Chill'
+    puts '[5] Exit'
+    puts 'Enter a number: '
+    end
+
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_field
+    when 2 then harvest
+    when 3 then status
+    when 4 then relax
+    when 5 then exit(0) # can also do else exit as the last condition, up to programmer
+    end
+  end
+end
+
+farm = Farm.new
+app = App.new("This is my shitty farm")
+
+app.main_menu
+app.print_main_menu
--- farm.rb
+++ farm.rb
@@ -1,5 +1,47 @@
 require_relative 'field'
+require 'pry'
 class Farm
+  attr_accessor :fields, :total_harvest
 
+  def initialize
+    @fields = []
+    @total_harvest = 0
+  end
 
+  def add_field(field)
+    @fields << field 
+  end
+
+  def harvest
+    binding.pry
+    @fields.each do |field|
+      puts "Harvesting #{field.production} from #{field.size} hectare #{field.type} field"
+      puts "****************************************"
+      @total_harvest += field.production
+    end
+    puts "The Farm has colleted a total of #{total_harvest} food from all the fields"
+    total_harvest
+  end
+
+  def status
+    @fields.each do |field|
+      puts "#{field.type} is #{field.size} hectares"
+    end
+    puts "The farm has #{@total_harvest}"
+  end
+
+  def relax
+
+    corn_total_size = 0
+    wheat_total_size = 0
+
+    fields.each do |field|
+      if field.type == "corn"
+        corn_total_size += field.size
+      else
+        wheat_total_size += field.size
+      end
+    end
+    puts "#{corn_total_size} hectares of tall green stalks rustling in the breeze fill your horizon. The sun hangs low, casting an orange glow on a sea of #{wheat_total_size} hectares of wheat."
+  end
 end
